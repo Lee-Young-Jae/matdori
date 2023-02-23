35287798e640baa8c3ce41f7c03748bfde36cc6b
Lee-Young-Jae <yjlee1937@naver.com>
2023-02-23 22:29:48 +0900

    feat: useStore 훅스 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
-import Header from '@/components/section1/Header';
+import Header from '@/components/common/Header';
 import Link from 'next/link';
 import styles from '../styles/header.module.scss';
+import { AiOutlineShareAlt } from 'react-icons/ai';
+import { VscFeedback } from 'react-icons/vsc';
+import MapSection from '@/components/home/MapSection';
+import { Store } from '@/types/store';
+import { NextPage } from 'next';
+import useStores from '@/hooks/useStores';
+import { useEffect } from 'react';
 
-export default function Home() {
+interface Props {
+  stores: Store[];
+}
+
+const Home: NextPage<Props> = ({ stores }) => {
+  const { initializeStores } = useStores();
+
+  useEffect(() => {
+    initializeStores(stores);
+  }, [stores, initializeStores]);
+
   return (
     <>
       <Header
@@ -15,12 +32,35 @@ export default function Home() {
             className={styles.box}
             style={{ marginRight: '8px' }}
           >
-            복사
+            <AiOutlineShareAlt />
           </button>,
-          <Link href="/feedback" key="link" className={styles.box}></Link>,
+          <Link href="/feedback" key="link" className={styles.box}>
+            <VscFeedback />
+          </Link>,
         ]}
       ></Header>
-      <main></main>
+      <main style={{ width: '100%', height: '100%' }}>
+        <MapSection />
+      </main>
     </>
   );
-}
+};
+
+export default Home;
+
+// getStaticProps
+export const getStaticProps = async () => {
+  /**
+   * TODO: Next API Routes로 불러오기
+   */
+  const stores = (await import('../public/stores.json')).default;
+
+  // json 파일을 불러올때는 import를 사용한다.
+  // default를 사용하는 이유는 json 파일을 불러올때는 default로 불러와야한다.
+  return {
+    props: {
+      stores,
+    },
+    revalidate: 60 * 60, // 1시간
+  };
+};
