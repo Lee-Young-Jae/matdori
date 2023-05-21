2eec0664e1163ea8ab4f2d9d91b5362e03c710fe
Lee-Young-Jae <yjlee1937@naver.com>
2023-05-21 18:57:29 +0900

    feat: 지도 URL 공유 기능 구현

--- components/home/MapSection.tsx
+++ components/home/MapSection.tsx
@@ -1,18 +1,48 @@
 import Map from './Map';
-import useMap from '@/hooks/useMap';
+import useMap, { INITIAL_CENTER, INITIAL_ZOOM } from '@/hooks/useMap';
 import { NaverMap } from '@/types/map';
-import React from 'react';
+import React, { useMemo } from 'react';
+import useCurrentStore from '@/hooks/useCurrentStore';
 import Markers from './Markers';
+import { useRouter } from 'next/router';
+import { Coordinates } from '@/types/store';
 
 const MapSection = () => {
+  /** initial zoom, center from url query */
+  const router = useRouter();
+  const query = useMemo(
+    () => new URLSearchParams(router.asPath.slice(1)), // slice로 / 문자 제거
+    [router.asPath]
+  );
+
+  const initialZoom = useMemo(
+    () => (query.get('zoom') ? Number(query.get('zoom')) : INITIAL_ZOOM),
+    [query]
+  );
+  const initialCenter = useMemo<Coordinates>(
+    () =>
+      query.get('lat') && query.get('lng')
+        ? [Number(query.get('lat')), Number(query.get('lng'))]
+        : INITIAL_CENTER,
+    [query]
+  );
+
+  /** onLoadMap */
   const { initializeMap } = useMap();
+  const { clearCurrentStore } = useCurrentStore();
+
   const onLoadMap = (map: NaverMap) => {
     initializeMap(map);
+    naver.maps.Event.addListener(map, 'click', clearCurrentStore);
   };
 
   return (
     <>
-      <Map onLoad={onLoadMap}></Map>
+      <Map
+        onLoad={onLoadMap}
+        initialZoom={initialZoom}
+        initialCenter={initialCenter}
+      ></Map>
       <Markers />
     </>
   );
