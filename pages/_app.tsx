649a3059ff408d7487ea0d4363b0cdc9475cbbc8
Lee-Young-Jae <yjlee1937@naver.com>
2023-06-09 00:25:14 +0900

    feat: 구글 애널리틱스 스크립트 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,30 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../seo.config';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-3FPEN2F9GF`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3FPEN2F9GF');
+            `,
+        }}
+      />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+
       <Component {...pageProps} />
     </>
   );
